107ec4e81982f8eb6e7d7c2d5588f35636d086b2
Shreyansh Jain <jshreyansh962@gmail.com>
2026-07-24 11:33:01 +0530

    feat: add Credential model class

---
+++ src/Credential.java
@@ -0,0 +1,43 @@
+public class Credential {
+
+  private String website;
+  private String username;
+  private String password;
+
+  public Credential(String website, String username, String password) {
+    this.website = website;
+    this.username = username;
+    this.password = password;
+  }
+
+  public String getWebsite() {
+    return website;
+  }
+  
+  public void setWebsite(String website) {
+    this.website = website;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+
+  @Override
+  public String toString() {
+    return "Website : " + website +
+           "\nUsername: " + username +
+           "\nPassword: " + password;
+  }
+}
